247cfc4e93b4406b4d104e23a3df30dec9b59351
Vaishali Mandora <vaishalimandora12@gmail.com>
2025-03-28 23:32:39 +0530

    v1

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -6,27 +6,7 @@ export interface user {
     lastName?: string;
     password?: string;
     email?: string;
-    countryCode?: string;
-    phoneNumber?: string;
     userId?: number;
     socialLogin?: boolean;
     status?: string;
-    address?: {
-        addressLine1?: string;
-        addressLine2?: string;
-        city?: string;
-        state?: string;
-        zipCode?: string;
-        location?: {
-            type?: string;
-            coordinates?: any[];
-        };
-    };
-    phone_otp_verfied?: boolean;
-    email_otp_verfied?: boolean;
-    isActive?: boolean;
-    isDeleted?: boolean;
-    profileImage?: any;
-    userProfileUpdate?: any;
-    gender: string;
 }
